f0253460b6b12f6377269aa27c6dd4f3396fd0ae
mohammed sottou <mohammedsottou@gmail.com>
2025-11-07 12:30:47 +0100

    add the index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
 import express from 'express';
+// import moduleRoute from "./routes/moduleRoute.js";
+import examenRoute from "./routes/examRoute.js";
+
+
+
 
 const app = express();
 app.use(express.json());
+
+// Module routes
+//app.use("/api/modules", moduleRoute);
+
+// Examen routes
+app.use("/api/examens", examenRoute);
 
 const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
     console.log(`Serveur en écoute sur http://localhost:${PORT}`);
 });
+
+
+
+
+
+
+
+
