f5eb38814721e9102799cd0b0a200601d7fedd4c
mohammed sottou <mohammedsottou@gmail.com>
2025-11-07 17:18:13 +0100

    i fix an error in src/index.ts (exam)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import examenRoute from "./routes/examRoute.js";
 
 const app = express();
 app.use(express.json());
+
+// Examen routes
+app.use("/api/examens", examenRoute);
 
 const PORT = process.env.PORT || 8081;
 
