1f6598f2280a3cd0fb1595d93a73e4f10939c815
fatimazahra-12 <fatimazahrabourajlate@gmail.com>
2025-11-16 21:59:40 +0100

    Add candidat controller route type + update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import candidatRoutes from "../src/routes/candidatRoute.js"
 
 const app = express();
 app.use(express.json());
+
+app.use("/api/candidatures", candidatRoutes)
 
 const PORT = process.env.PORT || 8081;
 
