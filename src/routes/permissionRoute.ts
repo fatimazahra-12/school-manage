16ba690d639b3dacc31f5dc890b4967936b0de18
fatimazahra-12 <fatimazahrabourajlate@gmail.com>
2025-11-16 23:08:05 +0100

    Update permissionRoute

--- src/routes/permissionRoute.ts
+++ src/routes/permissionRoute.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { getAllPermissions, getPermissionById, createPermission, updatePermission, deletePermission } from "../controllers/permissionController.js";
+import { checkPermission } from "../middlewares/roleMiddleware.js";
 
 const router = Router();
 
-router.get("/", getAllPermissions);
-router.get("/:id", getPermissionById);
-router.post("/", createPermission);
-router.put("/:id", updatePermission);
-router.delete("/:id", deletePermission);
+router.get("/", checkPermission("PERMISSION_MANAGE"), getAllPermissions);
+router.get("/:id", checkPermission("PERMISSION_MANAGE"), getPermissionById);
+router.post("/", checkPermission("PERMISSION_MANAGE"), createPermission);
+router.put("/:id", checkPermission("PERMISSION_MANAGE"), updatePermission);
+router.delete("/:id", checkPermission("PERMISSION_MANAGE"), deletePermission);
 
 export default router;
