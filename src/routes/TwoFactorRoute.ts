5043b686c815eefa8efd2b1fb052376b350654be
fatimazahra-12 <fatimazahrabourajlate@gmail.com>
2025-11-18 19:54:03 +0100

    Add 2F route

--- src/routes/TwoFactorRoute.ts
+++ src/routes/TwoFactorRoute.ts
@@ -5,7 +5,6 @@ const router = Router();
 
 router.post('/enable-2fa', TwoFactorController.enable2FA);
 router.post('/verify-2fa', TwoFactorController.verify2FA);
-
 router.get("/qrcode", TwoFactorController.qrCode);
 
 
