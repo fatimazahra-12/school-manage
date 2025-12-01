8d6d5867af961c4f855fdc0b3593871d5d12a07e
fatimazahra-12 <fatimazahrabourajlate@gmail.com>
2025-12-01 09:44:44 +0100

    solve some errors

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,19 +1,11 @@
-import dotenv from "dotenv";
-import path from "path";
+import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
-
-const envPath = path.resolve(process.cwd(), ".env");
-dotenv.config({ path: envPath });
-
-console.log("Loaded .env from:", envPath);
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
-  engine: "classic",
   datasource: {
     url: env("DATABASE_URL"),
   },
