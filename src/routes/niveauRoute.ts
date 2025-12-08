aab87a6a4b4cbb2ffc43ee2bace449791c53ce26
Taoufyq <toubou12@gmail.com>
2025-12-09 00:35:43 +0100

    niveau handling including advanced functionnalities

--- src/routes/niveauRoute.ts
+++ src/routes/niveauRoute.ts
@@ -4,16 +4,24 @@ import {
   getNiveauById,
   createNiveau,
   updateNiveau,
-  deleteNiveau
-} from '../controllers/niveauController';
+  deleteNiveau,
+  getNiveauxByFiliere,
+  getNiveauWithGroupes,
+  searchNiveaux
+} from "../controllers/niveauController.js";
+import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { checkPermission } from '../middlewares/roleMiddleware.js';
 
 const router = express.Router();
 
-// Base path: /api/niveaux
-router.get('/', getAllNiveaux);
-router.get('/:id', getNiveauById);
-router.post('/', createNiveau);
-router.put('/:id', updateNiveau);
-router.delete('/:id', deleteNiveau);
+router.get('/search', authMiddleware, checkPermission('NIVEAUX_VIEW'), searchNiveaux);
+router.get('/filiere/:filiere_id', authMiddleware, checkPermission('NIVEAUX_VIEW'), getNiveauxByFiliere);
+router.get('/:id/groupes', authMiddleware, checkPermission('NIVEAUX_VIEW'), getNiveauWithGroupes);
+
+router.get('/', authMiddleware, checkPermission('NIVEAUX_VIEW'), getAllNiveaux);
+router.get('/:id', authMiddleware, checkPermission('NIVEAUX_VIEW'), getNiveauById);
+router.post('/', authMiddleware, checkPermission('NIVEAUX_MANAGE'), createNiveau);
+router.put('/:id', authMiddleware, checkPermission('NIVEAUX_MANAGE'), updateNiveau);
+router.delete('/:id', authMiddleware, checkPermission('NIVEAUX_MANAGE'), deleteNiveau);
 
 export default router;
